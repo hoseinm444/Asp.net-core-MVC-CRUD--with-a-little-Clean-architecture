3adafd2fe418667c812a652e12d1ab4b65d16e82
Hoseinm444 <51280239+hoseinm444@users.noreply.github.com>
2023-10-26 10:47:28 +0330

    Add files via upload

---
+++ src/MVC/wwwroot/js/site.js
@@ -0,0 +1,25 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+//$(document).ready(function () {
+//    $('#tbllist').DataTable({
+//        processing: true,
+//        ordering: true,
+//        paging: true,
+//        searching: true,
+//        ajax:
+//        {
+//            'url': '@Url.Action("CRUDCompany", "Companies")'
+//        },
+//        columns: [
+//            { "data": "productId" },
+//            { "data": "name" },
+//            { "data": "productNumber" },
+//            { "data": "color" },
+//            { "data": "listPrice" }
+//        ]
+//    });
+//});
